61ce35c612b96864634a72dcdea8f6c9bf15318d
Tianzi Cai <anguillanneuf@gmail.com>
2019-09-04 13:44:03 -0700

    change outside function to sync

--- samples/vision/automlVisionPredict.js
+++ samples/vision/automlVisionPredict.js
@@ -22,7 +22,7 @@
 
 'use strict';
 
-async function predict(
+sync function predict(
   projectId,
   computeRegion,
   modelId,
@@ -30,54 +30,53 @@ async function predict(
   scoreThreshold
 ) {
   // [START automl_vision_predict]
-  const automl = require('@google-cloud/automl').v1beta1;
-  const fs = require('fs');
+  async function automlVisionPredict() {
+    const automl = require('@google-cloud/automl').v1beta1;
+    const fs = require('fs');
 
-  // Create client for prediction service.
-  const client = new automl.PredictionServiceClient();
+    // Create client for prediction service.
+    const client = new automl.PredictionServiceClient();
 
-  /**
-   * TODO(developer): Uncomment the following line before running the sample.
-   */
-  // const projectId = `The GCLOUD_PROJECT string, e.g. "my-gcloud-project"`;
-  // const computeRegion = `region-name, e.g. "us-central1"`;
-  // const modelId = `id of the model, e.g. “ICN12345”`;
-  // const filePath = `local text file path of content to be classified, e.g. "./resources/test.txt"`;
-  // const scoreThreshold = `value between 0.0 and 1.0, e.g. "0.5"`;
+    /**
+     * TODO(developer): Uncomment the following line before running the sample.
+     */
+    // const projectId = `The GCLOUD_PROJECT string, e.g. "my-gcloud-project"`;
+    // const computeRegion = `region-name, e.g. "us-central1"`;
+    // const modelId = `id of the model, e.g. “ICN12345”`;
+    // const filePath = `local text file path of content to be classified, e.g. "./resources/test.txt"`;
+    // const scoreThreshold = `value between 0.0 and 1.0, e.g. "0.5"`;
 
-  // Get the full path of the model.
-  const modelFullId = client.modelPath(projectId, computeRegion, modelId);
+    // Get the full path of the model.
+    const modelFullId = client.modelPath(projectId, computeRegion, modelId);
 
-  // Read the file content for prediction.
-  const content = fs.readFileSync(filePath, 'base64');
+    // Read the file content for prediction.
+    const content = fs.readFileSync(filePath, 'base64');
 
-  const params = {};
+    const params = {};
 
-  if (scoreThreshold) {
-    params.score_threshold = scoreThreshold;
-  }
+    if (scoreThreshold) {
+      params.score_threshold = scoreThreshold;
+    }
 
-  // Set the payload by giving the content and type of the file.
-  const payload = {};
-  payload.image = {imageBytes: content};
+    // Set the payload by giving the content and type of the file.
+    const payload = {};
+    payload.image = {imageBytes: content};
 
-  let response;
-
-  // params is additional domain-specific parameters.
-  // currently there is no additional parameters supported.
-  (async () => {
-    [response] = await client.predict({
+    // params is additional domain-specific parameters.
+    // currently there is no additional parameters supported.
+    const [response] = await client.predict({
       name: modelFullId,
       payload: payload,
       params: params,
     });
-  })();
+    console.log(`Prediction results:`);
+    response.payload.forEach(result => {
+      console.log(`Predicted class name: ${result.displayName}`);
+      console.log(`Predicted class score: ${result.classification.score}`);
+    });
+  }
 
-  console.log(`Prediction results:`);
-  response.payload.forEach(result => {
-    console.log(`Predicted class name: ${result.displayName}`);
-    console.log(`Predicted class score: ${result.classification.score}`);
-  });
+  automlVisionPredict();
   // [END automl_vision_predict]
 }
 
