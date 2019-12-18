d397e7952c4b87eff5b1b48665f704c2acdc71e7
nnegrey <nnegrey@google.com>
2019-12-18 13:28:49 -0700

    Update tests and license headers

--- samples/test/deploy_model.test.js
+++ samples/test/deploy_model.test.js
@@ -1,17 +1,16 @@
-/**
- * Copyright 2019 Google LLC
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 'use strict';
 
@@ -20,40 +19,32 @@ const {AutoMlClient} = require('@google-cloud/automl').v1;
 
 const cp = require('child_process');
 
-const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
+const fs = require('fs');
+
+const tempFile = 'deploy_model_output.txt';
+const stderr_output = fs.createWriteStream(tempFile);
+const execSync = cmd =>
+  cp.execSync(cmd, {encoding: 'utf-8', stdio: ['pipe', 'pipe', stderr_output]});
 
 const DEPLOY_MODEL_REGION_TAG = 'deploy_model';
 const LOCATION = 'us-central1';
-const MODEL_ID = 'TEN2238627664384491520';
+const MODEL_ID = 'TEN0000000000000000000';
 
 describe('Automl Deploy Model Test', () => {
   const client = new AutoMlClient();
 
-  before('should verify the model is not deployed', async () => {
-    const projectId = await client.getProjectId();
-    const request = {
-      name: client.modelPath(projectId, LOCATION, MODEL_ID),
-    };
-
-    const [response] = await client.getModel(request);
-    if (response.deploymentState === 'DEPLOYED') {
-      const request = {
-        name: client.modelPath(projectId, LOCATION, MODEL_ID),
-      };
-
-      const [operation] = await client.undeployModel(request);
-
-      // Wait for operation to complete.
-      await operation.promise();
-    }
-  });
-
   it('should deploy a model', async () => {
+    // As model deployment can take a long time, instead try to deploy a
+    // nonexistent model and confirm that the model was not found, but other
+    // elements of the request were valid.
     const projectId = await client.getProjectId();
-    const deploy_output = execSync(
+    execSync(
       `node ${DEPLOY_MODEL_REGION_TAG}.js ${projectId} ${LOCATION} ${MODEL_ID}`
     );
 
-    assert.match(deploy_output, /Model deployment finished./);
+    const contents = fs.readFileSync(tempFile, 'utf8');
+    assert.match(contents, /NOT_FOUND/);
+    assert.match(contents, /The model does not exist./);
+    fs.unlinkSync(tempFile);
   });
 });
--- samples/test/undeploy_model.test.js
+++ samples/test/undeploy_model.test.js
@@ -1,17 +1,16 @@
-/**
- * Copyright 2019 Google LLC
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 'use strict';
 
@@ -20,40 +19,32 @@ const {AutoMlClient} = require('@google-cloud/automl').v1;
 
 const cp = require('child_process');
 
-const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
+const fs = require('fs');
 
-const UNDEPLOY_MODEL_REGION_TAG = 'undeploy_model';
+const tempFile = 'undeploy_model_output.txt';
+const stderr_output = fs.createWriteStream(tempFile);
+const execSync = cmd =>
+  cp.execSync(cmd, {encoding: 'utf-8', stdio: ['pipe', 'pipe', stderr_output]});
+
+const DEPLOY_MODEL_REGION_TAG = 'undeploy_model';
 const LOCATION = 'us-central1';
-const MODEL_ID = 'TCN7483069430457434112';
+const MODEL_ID = 'TEN0000000000000000000';
 
 describe('Automl Undeploy Model Test', () => {
   const client = new AutoMlClient();
 
-  before('should verify the model is deployed', async () => {
-    const projectId = await client.getProjectId();
-    const request = {
-      name: client.modelPath(projectId, LOCATION, MODEL_ID),
-    };
-
-    const [response] = await client.getModel(request);
-    if (response.deploymentState === 'UNDEPLOYED') {
-      const request = {
-        name: client.modelPath(projectId, LOCATION, MODEL_ID),
-      };
-
-      const [operation] = await client.deployModel(request);
-
-      // Wait for operation to complete.
-      await operation.promise();
-    }
-  });
-
   it('should undeploy a model', async () => {
+    // As model undeployment can take a long time, instead try to undeploy a
+    // nonexistent model and confirm that the model was not found, but other
+    // elements of the request were valid.
     const projectId = await client.getProjectId();
-    const deploy_output = execSync(
-      `node ${UNDEPLOY_MODEL_REGION_TAG}.js ${projectId} ${LOCATION} ${MODEL_ID}`
+    execSync(
+      `node ${DEPLOY_MODEL_REGION_TAG}.js ${projectId} ${LOCATION} ${MODEL_ID}`
     );
 
-    assert.match(deploy_output, /Model undeployment finished./);
+    const contents = fs.readFileSync(tempFile, 'utf8');
+    assert.match(contents, /NOT_FOUND/);
+    assert.match(contents, /The model does not exist./);
+    fs.unlinkSync(tempFile);
   });
 });
