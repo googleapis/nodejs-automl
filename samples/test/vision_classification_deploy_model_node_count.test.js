cba345b432411ed0ef51d5a81f1efc990c97d35c
nnegrey <nnegrey@google.com>
2020-01-02 09:25:18 -0700

    Lint and license header fixes

--- samples/test/vision_classification_deploy_model_node_count.test.js
+++ samples/test/vision_classification_deploy_model_node_count.test.js
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,11 +15,13 @@
 'use strict';
 
 const {assert} = require('chai');
+const {describe, it} = require('mocha');
 const {AutoMlClient} = require('@google-cloud/automl').v1;
 
 const cp = require('child_process');
 
-const DEPLOY_MODEL_REGION_TAG = 'vision_classification_deploy_model_node_count.js';
+const DEPLOY_MODEL_REGION_TAG =
+  'vision_classification_deploy_model_node_count.js';
 const LOCATION = 'us-central1';
 const MODEL_ID = 'ICN0000000000000000000';
 
@@ -32,7 +34,7 @@ describe('Automl Vision Classification Deploy Model Test', () => {
     // elements of the request were valid.
     const projectId = await client.getProjectId();
     const args = [DEPLOY_MODEL_REGION_TAG, projectId, LOCATION, MODEL_ID];
-    const output = cp.spawnSync('node', args, { encoding : 'utf8' });
+    const output = cp.spawnSync('node', args, {encoding: 'utf8'});
 
     assert.match(output.stderr, /NOT_FOUND/);
     assert.match(output.stderr, /The model does not exist./);
--- samples/test/vision_classification_predict.test.js
+++ samples/test/vision_classification_predict.test.js
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 'use strict';
 
 const {assert} = require('chai');
+const {before, describe, it} = require('mocha');
 const {AutoMlClient} = require('@google-cloud/automl').v1;
 
 const cp = require('child_process');
